04b2e8fd776f2e723c1ace9b5c6cc8e0c38cb02e
SCE <sce@gglobaz.ch>
2018-10-23 09:37:19 +0200

    initial commit

--- src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
+++ src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
@@ -18,7 +18,7 @@ public class CatalogController {
 
         RestTemplate restTemplate = new RestTemplate();
         String userResource
-                = "http://node4023/auth";
+                = "http://http://env-1024481.jcloud.ik-server.com//auth";
 
         HttpEntity<Login> request = new HttpEntity<>(login);
 
