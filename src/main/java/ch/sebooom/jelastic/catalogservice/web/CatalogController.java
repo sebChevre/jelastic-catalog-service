adf7df04f21351782b262bd512efcf41ff9aede2
SCE <sce@gglobaz.ch>
2018-10-23 17:22:18 +0200

    as

--- src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
+++ src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
@@ -1,17 +1,27 @@
 package ch.sebooom.jelastic.catalogservice.web;
 
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RestController
 @RequestMapping("/catalog/login")
 public class CatalogController {
 
+    @Value("${USERS_ENV_MASTER_IP}")
+    private String USERS_URI;
+
     @PostMapping
     public ResponseEntity login(@RequestBody Login login){
+
+        log.info("Service uri: {}",USERS_URI);
 
         RestTemplate restTemplate = new RestTemplate();
         String userResource
