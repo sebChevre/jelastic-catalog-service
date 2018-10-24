e64e1b75dc9790f95452844be9feb758616e4c6d
SCE <sce@gglobaz.ch>
2018-10-24 14:43:34 +0200

    as

--- src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
+++ src/main/java/ch/sebooom/jelastic/catalogservice/web/CatalogController.java
@@ -23,8 +23,8 @@ public class CatalogController {
     @PostMapping
     public ResponseEntity login(@RequestBody Login login){
 
-        log.info("Service uri: {}",env.getProperty("JAVA_HOME"));
-        log.info("Service uri: {}",env.getProperty("USERS_MASTER_IP"));
+        log.info("Service uri: {}",env.getProperty("$JAVA_HOME"));
+        log.info("Service uri: {}",env.getProperty("$USERS_MASTER_IP"));
 
         RestTemplate restTemplate = new RestTemplate();
         String userResource
